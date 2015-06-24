3e54129a882b676dbfc388fe7e3115b265df1d0e
Brandon Espinoza <espinozab100@gmail.com>
2015-06-23 19:36:09 -0500

    Added comments.count and likes to index.html

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150623151512) do
+ActiveRecord::Schema.define(version: 20150623224856) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "catrgories", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "comments", force: :cascade do |t|
     t.text     "content"
@@ -37,6 +49,7 @@ ActiveRecord::Schema.define(version: 20150623151512) do
     t.string   "image_content_type"
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
+    t.integer  "category_id"
   end
 
   add_index "pins", ["user_id"], name: "index_pins_on_user_id", using: :btree
